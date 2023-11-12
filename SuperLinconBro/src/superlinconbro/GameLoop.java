bd50ef05d581c6fc08f982e112091ed8cc08eb07
samuelfabrasil <samuelfabrasil@gmail.com>
2023-11-12 18:06:35 -0300

    initial commit

---
+++ SuperLinconBro/src/superlinconbro/GameLoop.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superlinconbro;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Samuel
+ */
+public class GameLoop extends  JPanel{
+    private int width;
+    private int heigth;
+    
+    public GameLoop(){
+       
+    }
+    
+    @Override
+    public void paint(Graphics g){
+
+    }
+    
+    public void gamelLogic(ArrayList<Integer>input){
+        
+    }
+    
+    public void restart(){
+
+    }
+
+}
---
+++ SuperLinconBro/src/superlinconbro/SuperLinconBro.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package superlinconbro;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author Samuel
+ */
+public class SuperLinconBro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame janela = new  JFrame("SuperLinconBros");
+        janela.setSize(600, 600);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+        GameLoop game = new GameLoop();
+        janela.add(game);
+        ArrayList<Integer> input = new ArrayList();     
+        janela.addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent e) {
+                int keycode = e.getExtendedKeyCode();
+                if(!input.contains(keycode))
+                    input.add(keycode);
+            }
+            public void keyReleased(KeyEvent e) {
+                int keycode = e.getExtendedKeyCode();
+                int ind = input.indexOf(keycode);
+                input.remove (ind);
+            }
+        });
+        Timer timer = new Timer(66, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.gamelLogic(input);
+                game.repaint();
+                input.forEach((x)->System.out.print(x));
+            }
+        });
+    }
+    
+}
