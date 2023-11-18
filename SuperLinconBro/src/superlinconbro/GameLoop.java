48ba268bd780c326bdab978177a97a1d72de7177
samuelfabrasil <samuelfabrasil@gmail.com>
2023-11-18 16:31:41 -0300

    mudando o loop principal

--- SuperLinconBro/src/superlinconbro/GameLoop.java
+++ SuperLinconBro/src/superlinconbro/GameLoop.java
@@ -4,7 +4,9 @@
  */
 package superlinconbro;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
@@ -15,18 +17,21 @@ import javax.swing.JPanel;
 public class GameLoop extends  JPanel{
     private int width;
     private int heigth;
-    
-    public GameLoop(){
-       
+    private Mario mario;
+    public GameLoop() throws IOException{
+       this.mario = new Mario();
     }
     
     @Override
     public void paint(Graphics g){
-
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 600, 600);
+        g.setColor(Color.black);
+        mario.paint(g);
     }
     
     public void gamelLogic(ArrayList<Integer>input){
-        
+        mario.move(input,0);
     }
     
     public void restart(){
