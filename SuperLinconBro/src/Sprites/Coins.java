ff1b8c5e2157913b27f4003e6cf589b3f2513adc
Hubert <hubertluzm@gmail.com>
2023-11-19 19:09:11 -0300

    Atualizada a classe Coins

--- SuperLinconBro/src/Sprites/Coins.java
+++ SuperLinconBro/src/Sprites/Coins.java
@@ -5,20 +5,21 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import superlinconbro.GameLoop;
 
 public class Coins {
     private int x, y;
     private static final int xe = 20, ye = 50; // Coordenadas da moeda estática (irão ser alteradas)
     private Image[] images;
-    private int currentFrame; // Índice da imagem atual
-    private int totalFrames; // Número total de frames na animação
-    private int delay; // Atraso entre os frames
-    private long lastTimeChange; // Tempo da última mudança de frame
+    private int currentFrame;
+    private int totalFrames;
+    private int delay;
+    private long lastTimeChange;
     private static int counter;
     private Image coinImage;
 
-    private static final int AMPLITUDE = 20; // Amplitude da animação
-    private static final int PERIOD = 1000; // Período da animação em milissegundos
+    private static final int amplitude = 20; // Amplitude da animação
+    private static final int period = 1000; // Período da animação em milissegundos
 
     public Coins(int x, int y) {
         this.x = x;
@@ -28,37 +29,32 @@ public class Coins {
 
         images = new Image[4];
         for (int i = 0; i < images.length; i++) {
-            ImageIcon ii = new ImageIcon("caminho/para/imagem/" + i + ".png");
+            ImageIcon ii = new ImageIcon("src/Sprites/coin" + i + ".png");
             images[i] = ii.getImage();
         }
         totalFrames = images.length;
         lastTimeChange = System.currentTimeMillis();
 
-        ImageIcon coinIcon = new ImageIcon("caminho/para/imagem/moeda_estatica.png");
+        ImageIcon coinIcon = new ImageIcon("src/Sprites/coin4.png");
         coinImage = coinIcon.getImage();
     }
 
     private void animation() {
         long elapsedTime = System.currentTimeMillis() - lastTimeChange;
 
-        // Calcula a posição vertical baseada no tempo
-        int offsetY = (int) (AMPLITUDE * Math.sin(2 * Math.PI * elapsedTime / PERIOD));
+        int offsetY = (int) (amplitude * Math.sin(2 * Math.PI * elapsedTime / period));
 
-        // Atualiza a posição Y da moeda animada
         y += offsetY;
     }
 
     public void draw(Graphics g) {
-        // Chama o método de animação
+
         animation();
 
-        // Desenha a moeda estática ao lado do contador
         g.drawImage(coinImage, xe, ye, null);
 
-        // Desenha a moeda animada
         g.drawImage(images[currentFrame], x, y, null);
 
-        // Desenha o contador na tela
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 16));
         g.drawString("X" + counter, xe + 5, ye);
@@ -79,7 +75,7 @@ public class Coins {
         return y;
     }
 
-    public int getContador() {
+    public int getCounter() {
         return counter;
     }
 }
