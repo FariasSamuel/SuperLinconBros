c66e2014249b7a0bc514a08441ae9e4c969a98ef
Mateus <matribg04@gmail.com>
2023-11-30 16:53:37 -0300

    Atualizei alguns metodos

--- SuperLinconBro/src/superlinconbro/Koopa.java
+++ SuperLinconBro/src/superlinconbro/Koopa.java
@@ -25,23 +25,57 @@ public class Koopa extends Enemies {
     private char lastKey;
 
     public Koopa() throws IOException {
+        
+        String path = new File("src/Sprites/Koopa.png").getAbsolutePath();
+        this.setImage(ImageIO.read(new File(path)));
     }
 
 
     
     @Override
     public void draw(Graphics g){
-           
+           int startX = this.getFramex()* (150 + 60);
+        int startY = this.getFramey() * 240;
+        int endX = (this.getFramex() + 1) * (150 + 60) - 60;
+        int endY = (this.getFramey() + 1) * 240;
+
+        g.drawImage(
+            this.getImage(),
+            this.getX(),
+            this.getY(),
+            this.getX() + this.getWidth(),
+            this.getY() + this.getHeight(),
+            startX, startY, endX, endY,
+            null
+        );
     }
+    
     @Override
     public void update(int speed, int speedy, int time, double deltaTime) {
         this.setX(this.getX()-this.getSpeed());
         this.setY(this.getY()-this.speedy + this.getSpeed());
         this.setOriginY(this.getOriginY()-this.speedy);
         
-        
-        
-        
+        if(this.getOriginY() - this.getY() > 200){
+            this.setSpeed(-5);
+            
+            if(this.getMario().getX() + this.getGame().getCameraX() > this.getX()+ this.getGame().getCameraX() - 400){
+                this.setFramey(0) ;
+            } 
+            else{
+                this.setFramey(3);
+            }
+        }else if(this.getOriginY() - this.getY() < 1){
+            this.setSpeed(5);
+            
+            if(this.getMario().getX() + this.getGame().getCameraX() > this.getX()+ this.getGame().getCameraX() - 400){
+                this.setFramey(1);
+            }else{
+                this.setFramey(4);
+            }
+        }
+        this.setMaxFrame(3);
+        this.setFramex(0);
         this.collision();
         this.animation(deltaTime);
         this.attack(time, deltaTime);
